1189699fdc88a5bc90013e0c995c32d606fa4b94
Паша <bps191342@gmail.com>
2025-11-06 23:01:24 +0300

    Глава 1 из книги Доусона

---
+++ stepik_basics/tasks/glava_1.py
@@ -0,0 +1,10 @@
+# Game Over
+# Демонстрирует работу функции print
+print('Game Over')
+input('\n\nНажмите Enter, чтобы выйти') # отступаем 2 строки
+name = input('\n\nВведите имя: ')
+print(name)
+input('Нажмите Enter, чтобы выйти')
+text = input('\n\nВведите фразу: ')
+author = input('Введите автора: ')
+print(text,'\n',author)
